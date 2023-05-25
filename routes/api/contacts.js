5a4e46d2aee483e308cf0f247a32b663e958c592
SergSmilya <sergsmilichenko@gmail.com>
2023-05-25 19:36:25 +0300

    add some function REST API

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,94 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require("../../models/contacts");
+const HttpError = require("../../helpers");
+const shema = require("../../shemas/contacts");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    res.status(200).json(await listContacts());
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const result = await getContactById(contactId);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.status(200).json(result);
+  } catch (error) {
+    const { status = 500, message = "Server error" } = error;
+    res.status(status).json({ message });
+  }
+});
 
-module.exports = router
+router.post("/", async (req, res, next) => {
+  try {
+    const { error } = shema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+
+    // ! зробити валідацію правильною (номер це намберб, імеіл це імеіл)
+    // ! розібратися що саме записати в error.message згідно тех.завдання
+
+    const result = await addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    const { status = 500, message = "Server error" } = error;
+    res.status(status).json({ message });
+  }
+});
+
+router.delete("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+
+  try {
+    const result = await removeContact(contactId);
+
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.status(200).json({ message: "contact deleted" });
+  } catch (error) {
+    const { status = 500, message = "Server error" } = error;
+    res.status(status).json({ message });
+  }
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  // ? Якщо body немає, повертає json з ключем {"message": "missing fields"} і статусом 400
+  try {
+    const { error } = shema.validate(req.body);
+
+    if (error) {
+      throw HttpError(400, "missing fields");
+    }
+
+    // ? За результатом роботи функції повертає оновлений об'єкт контакту і статусом 200. В іншому випадку, повертає json з ключем "message": "Not found" і статусом 404
+    const result = await updateContact(contactId, req.body);
+    res.status(200).json(result);
+  } catch (error) {
+    const { status = 500, message = "Server error" } = error;
+    res.status(status).json({ message });
+  }
+});
+
+module.exports = router;
+
+// ? Розділити по файлам, уникнути дублювання коду, рефакторинг
