bf25a97edfe4a8de783a9aa01e301d2613055f26
SergSmilya <sergsmilichenko@gmail.com>
2023-06-10 14:36:09 +0300

    finished, but tests not worked

--- tests/login.test.js
+++ tests/login.test.js
@@ -2,11 +2,15 @@
 // ! у відповіді повинен повертатися токен
 // ! у відповіді повинен повертатися об'єкт user з 2 полями email и subscription з типом даних String
 
-const login = require("../controllers/auth");
+const { login } = require("../controllers/auth");
+const request = require("supertest");
+const app = require("../app");
 
-describe("ctrl - login", () => {
+describe("post /login", () => {
+  beforeAll(() => app.listen(3000));
+
   test("відповідь повина мати статус-код 200", async () => {
-    const result = await login(req, res);
-    expect(result).toBe({ status: 200 });
+    const result = await request(app).post("/login", login);
+    expect(result.statusCode).toBe(200);
   });
 });
